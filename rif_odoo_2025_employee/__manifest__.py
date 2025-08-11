5975aa00d9d0b2d23295816fdb89b57ea04a3871
abdelhamid2506 <mohamedabdelhamidfarhat@gmail.com>
2025-08-11 16:20:33 +0100

    active/desactive attendance

--- rif_odoo_2025_employee/__manifest__.py
+++ rif_odoo_2025_employee/__manifest__.py
@@ -1,36 +1,32 @@
 {
-    "name":"rif_odoo_2025_employee_part_2025",
-    "sumarry":"building and testing rif_odoo_employee_part_2025 to be added in the official project",
-    "version":"1.0",
-    "license":":LGPL-3",
-    "depends":[],
-    "data":[
-        #security
-                #contrat
-                "security/contrat_access.xml",
-                #departement
-                "security/departement_access.xml",
-                #employee
-                "security/employee_access.xml",
-                #test_candidat
-                "security/test_candidat_access.xml",
-            "security/ir.model.access.csv",
-        #views
-            #contrat
-                "views/contrat_views.xml",
-                "views/contrat_menus.xml",
-            #departement
-                "views/departement_views.xml",
-                "views/departement_menus.xml",
-            #employee
-                "views/employee_views.xml",
-                "views/employee_menus.xml",
-            #test_candidat
-                "views/test_candidat_views.xml",
-                "views/test_candidat_menus.xml",
-            
-
+    "name": "rif_odoo_2025_employee_part_2025",
+    "summary": "Building and testing rif_odoo_employee_part_2025 to be added in the official project",
+    "version": "1.0",
+    "license": "LGPL-3",
+    "depends": [
+        #orm
+        "base",
+        #employee
+        "hr",
+        #contrat
+        "hr_contract", 
+            # Optional, for digital signatures
+        "sign",  
+        #equipements
+        "maintenance",
+        #skills and CV
+        "hr_skills",
+        "hr_skills_slides",
+        "hr_skills_survey",
+        # hr_attendance is the base presence module
+        'hr_attendance',
+        #to do list
+        "hr_timesheet",
+        
     ],
-    "demo":[],
-    "assets":{}
+    "data": [
+        
+    ],
+    "demo": [],
+    "assets": {}
 }
